ead5390e5ebf1a949d4773430fd0d22d5e957151
Nikita Chand <nikitachand754@gmail.com>
2026-03-31 17:48:38 +0530

    Update data_loader.py

--- data_loader.py
+++ data_loader.py
@@ -3,12 +3,16 @@ import cv2
 import numpy as np
 
 def load_frames(folder):
-    frames = []
+    data = []
+
     for file in os.listdir(folder):
         path = os.path.join(folder, file)
+
         img = cv2.imread(path)
+
         if img is not None:
             img = cv2.resize(img, (128, 128))
             img = img / 255.0
-            frames.append(img)
-    return np.array(frames)
+            data.append(img)
+
+    return np.array(data)
